88862db61204ccdf01a2c93e9c85099ac1454ca8
Stefan Miljevic <stefi5miljevic@gmail.com>
2021-03-17 23:26:54 +0100

    Add missing Update, Delete graphql mutations

--- thefrontpage-server/src/main/java/xyz/thefrontpage/graphql/resolver/mutation/MutationResolver.java
+++ thefrontpage-server/src/main/java/xyz/thefrontpage/graphql/resolver/mutation/MutationResolver.java
@@ -24,21 +24,54 @@ public class MutationResolver implements GraphQLMutationResolver {
     private final CommentService commentService;
     private final VoteService voteService;
 
+    /* COMMUNITY MUTATIONS */
     public Community createCommunity(CommunityRequest communityRequest) {
         return communityService.createCommunity(communityRequest);
     }
 
+    public boolean updateCommunity(CommunityRequest communityRequest) {
+        communityService.updateCommunity(communityRequest);
+        return true;
+    }
+
+    public boolean deleteCommunity(String name) {
+        communityService.deleteCommunity(name);
+        return true;
+    }
+
+    /* POST MUTATIONS */
     public Post createPost(PostRequest postRequest) {
         return postService.createPost(postRequest);
     }
 
+    public boolean updatePost(PostRequest postRequest) {
+        postService.updatePost(postRequest);
+        return true;
+    }
+
+    public boolean deletePost(Long postId) {
+        postService.deletePost(postId);
+        return true;
+    }
+
+    /* COMMENT MUTATIONS */
     public Comment createComment(CommentRequest commentRequest) {
         return commentService.createComment(commentRequest);
     }
 
+    public boolean updateComment(CommentRequest commentRequest) {
+        commentService.updateComment(commentRequest);
+        return true;
+    }
+
+    public boolean deleteComment(Long commentId) {
+        commentService.deleteComment(commentId);
+        return true;
+    }
+
+    /* VOTE MUTATIONS */
     public boolean vote(VoteRequest voteRequest) {
         voteService.vote(voteRequest);
         return true;
     }
-
 }
